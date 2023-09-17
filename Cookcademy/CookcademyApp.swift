4a69e6be5ecd99e3ad7ec6a950b9e41c2f7b0aa4
Andrew <66540891+andrewtyree@users.noreply.github.com>
2023-09-16 21:47:30 -0500

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Andrew Tyree on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
